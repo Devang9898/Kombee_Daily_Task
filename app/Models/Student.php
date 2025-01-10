505c258b6aad590dbeae86e6cc6430b2435d408f
Your Name <devangrjoshi2003@gmail.com>
2025-01-10 10:08:54 +0530

    crud operation and accessor and modifer

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -8,5 +8,70 @@ class Student extends Model
 {
     //
     protected $fillable = ['name', 'email', 'phone', 'dob'];
-    
+    // Accessor for name 
+    public function getNameAttribute($value)
+    {
+        return ucfirst($value);
+    }
+
+    // Mutator for email
+    public function setEmailAttribute($value)
+    {
+        $this->attributes['email'] = strtolower($value);
+    }
+
+    // Accessor for formatted date of birth
+    public function getDobAttribute($value)
+    {
+        return \Carbon\Carbon::parse($value)->format('d-M-Y');
+    }
+
+    // Mutator for phone
+    public function setPhoneAttribute($value)
+    {
+        // Storing phone number with only digits
+        $this->attributes['phone'] = preg_replace('/[^0-9]/', '', $value);
+    }
 }
+/*<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name', 'email', 'phone', 'dob'
+    ];
+
+    // Accessor for name 
+    public function getNameAttribute($value)
+    {
+        return ucfirst($value);
+    }
+
+    // Mutator for email
+    public function setEmailAttribute($value)
+    {
+        $this->attributes['email'] = strtolower($value);
+    }
+
+    // Accessor for formatted date of birth
+    public function getDobAttribute($value)
+    {
+        return \Carbon\Carbon::parse($value)->format('d-M-Y');
+    }
+
+    // Mutator for phone
+    public function setPhoneAttribute($value)
+    {
+        // Storing phone number with only digits
+        $this->attributes['phone'] = preg_replace('/[^0-9]/', '', $value);
+    }
+}
+?>
+*/
